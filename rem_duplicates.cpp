46251d08bb6e5bb61db47f5cdeb09b85d2d1737b
Azar Mohammad <97538308+21075A6603@users.noreply.github.com>
2025-06-24 19:39:15 +0530

    Create rem_duplicates.cpp
    In the initial approach, I utilized an auxiliary set to remove duplicates from the data. Since sets inherently do not allow duplicate values, this method effectively eliminated redundancy.
    
    However, in the second approach, I optimized the space usage by implementing a two-pointer technique within a single iteration loop. This allowed me to remove duplicates in-place, thereby avoiding the need for additional data structures.
    
    Although both approaches have the same time complexity, the second method significantly reduces the space complexity, making it a more efficient solution overall.

---
+++ rem_duplicates.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+void pointerApp(int arr[],int n){
+    int j=0;
+    for(int i=1;i<n;i++){
+        if(arr[j]!=arr[i]){
+            j++;
+            arr[j]=arr[i];
+        }
+    }
+    cout<<"finel array:"<<endl;
+    for(int i=0;i<j;i++){
+        cout<<arr[i]<<" ";
+    }
+    
+}
+int main(){
+    int arr[] = {1,1,2,2,2,3,3};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    set<int> s;
+    for(int i=0;i<n;i++){
+        s.insert(arr[i]);
+    }
+    int j=0;
+    for(auto x:s){
+        arr[j]=x;
+        j++;
+    }
+    s={};
+    cout<<"sise of the array:"<<j<<endl;
+    for(int i=0;i<j;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    cout<<endl;
+    cout<<"using two pointer approach:"<<endl;
+    pointerApp(arr,n);
+    
+    return 0;
+}
